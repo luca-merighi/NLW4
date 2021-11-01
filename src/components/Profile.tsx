c8e9c3db3d65e93c9059660fdd232a28aa714d18
Luca Merighi <73050019+luca-merighi@users.noreply.github.com>
2021-11-01 12:09:18 -0300

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://instagram.fcpq3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/242515706_927009401502968_5085950122251386387_n.jpg?_nc_ht=instagram.fcpq3-1.fna.fbcdn.net&_nc_ohc=4Ie8oFG7kikAX98KWO0&edm=ABfd0MgBAAAA&ccb=7-4&oh=ab6bc9895e11793d0378b0cad55692b6&oe=6152EF64&_nc_sid=7bff83" alt="Luca" />
+            <img src="https://avatars.githubusercontent.com/u/73050019?v=4" alt="Luca" />
 
             <div>
                 <strong>Luca Fernandes</strong>
